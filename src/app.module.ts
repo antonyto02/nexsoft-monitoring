4f4f602505500718baea330786ad273f3c894532
Antonio Delgado <tonydelro@gmail.com>
2025-06-10 12:55:43 -0600

    Conexion con BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://antonyto02:T6DfHvPfMLXLCu4k@nextsoft-cluster.k7wjxb9.mongodb.net/?retryWrites=true&w=majority&appName=nextsoft-cluster'),
+  ],
 })
 export class AppModule {}
